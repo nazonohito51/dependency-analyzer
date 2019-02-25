343d46a8386f91ad529e129f42c629d6a26a25a8
kawashima <nazonohito51@gmail.com>
2019-02-25 11:01:35 +0900

    add declare

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace Tests;
 
 abstract class TestCase extends \PHPUnit\Framework\TestCase
--- tests/Unit/DirectedGraphTest.php
+++ tests/Unit/DirectedGraphTest.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace Tests\Unit\DependencyAnalyzer;
 
 use DependencyAnalyzer\DirectedGraph;
