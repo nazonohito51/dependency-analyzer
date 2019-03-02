3a5715cd2e137eef8e3f9c70ea9769c6f59b90a3
kawashima <nazonohito51@gmail.com>
2019-03-02 15:43:52 +0900

    add tmp dir in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,8 +5,28 @@ namespace Tests;
 
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
-    public function getFixturePath($name)
+    /**
+     * @return string
+     */
+    protected function getRootDir(): string
     {
-        return __DIR__ . '/fixtures' . (substr($name, 0, 1) === '/' ? $name : "/{$name}");
+        return realpath(__DIR__ . '/../');
+    }
+
+    /**
+     * @param $name
+     * @return string
+     */
+    public function getFixturePath($name): string
+    {
+        return $this->getRootDir() . '/tests/fixtures' . (substr($name, 0, 1) === '/' ? $name : "/{$name}");
+    }
+
+    /**
+     * @return string
+     */
+    protected function getTmpDir(): string
+    {
+        return $this->getRootDir() . '/tests/tmp/';
     }
 }
