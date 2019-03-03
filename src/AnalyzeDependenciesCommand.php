9eb12128855ca721ca1f67bf7dd7206540afb04c
kawashima <nazonohito51@gmail.com>
2019-03-03 22:26:23 +0900

    refactor AnalyzeDependenciesCommand

--- src/AnalyzeDependenciesCommand.php
+++ src/AnalyzeDependenciesCommand.php
@@ -1,13 +1,15 @@
-<?php declare(strict_types = 1);
+<?php
+declare(strict_types = 1);
 
 namespace DependencyAnalyzer;
 
-use Nette\Utils\Json;
-use PHPStan\Command\CommandHelper;
-use PHPStan\Command\ErrorsConsoleStyle;
-use PHPStan\Dependency\DependencyDumper;
+use DependencyAnalyzer\Detector\CycleDetector;
+use DependencyAnalyzer\Exceptions\InvalidCommandArgumentException;
+use DependencyAnalyzer\Exceptions\UnexpectedException;
+use Nette\DI\Container;
+use PHPStan\DependencyInjection\ContainerFactory;
+use PHPStan\File\FileFinder;
 use PHPStan\File\FileHelper;
-use PHPStan\ShouldNotHappenException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -15,108 +17,141 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class AnalyzeDependenciesCommand extends \Symfony\Component\Console\Command\Command
 {
-    private const NAME = 'analyze-deps';
+    protected const NAME = 'analyze-deps';
+
+    /**
+     * @var string[]
+     */
+    protected $paths = [];
+
+    /**
+     * @var Container $container
+     */
+    protected $container;
 
     protected function configure(): void
     {
         $this->setName(self::NAME)
             ->setDescription('Analyze dependency tree')
             ->setDefinition([
-                new InputArgument('paths', InputArgument::OPTIONAL | InputArgument::IS_ARRAY, 'Paths with source code to run dump on'),
-                new InputOption('paths-file', null, InputOption::VALUE_REQUIRED, 'Path to a file with a list of paths to run analysis on'),
-                new InputOption('configuration', 'c', InputOption::VALUE_REQUIRED, 'Path to project configuration file'),
-                new InputOption(ErrorsConsoleStyle::OPTION_NO_PROGRESS, null, InputOption::VALUE_NONE, 'Do not show progress bar, only results'),
-                new InputOption('autoload-file', 'a', InputOption::VALUE_REQUIRED, 'Project\'s additional autoload file path'),
-                new InputOption('memory-limit', null, InputOption::VALUE_REQUIRED, 'Memory limit for the run'),
-                new InputOption('analysed-paths', null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED, 'Project-scope paths'),
+                new InputArgument('paths', InputArgument::REQUIRED | InputArgument::IS_ARRAY, 'Target directory of analyze'),
+                new InputOption('memory-limit', null, InputOption::VALUE_REQUIRED, 'Memory limit for the run (ex: 500k, 500M, 5G)'),
             ]);
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->paths = $this->getAbsolutePaths($input);
+        $this->container = $this->createContainer($this->paths);
+
+        if ($memoryLimit = $input->getOption('memory-limit')) {
+            $this->setMemoryLimit($memoryLimit);
+        }
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        try {
-            $inceptionResult = $this->buildInceptionResult($input, $output);
-        } catch (\PHPStan\Command\InceptionNotSuccessfulException $e) {
-            return 1;
+        $dependencyDumper = $this->container->getByType(DependencyDumper::class);
+        $allFiles = $this->getAllFilePaths();
+
+        $dependencies = $dependencyDumper->dump($allFiles);
+        
+        $errors = $this->verify($dependencies);
+        var_dump($errors);
+
+//        $output->writeln(Json::encode($dependencies, Json::PRETTY));
+        var_dump($dependencies->toArray());
+        var_dump(count($dependencies));
+        $count = 0;
+        foreach ($dependencies->toArray() as $depender => $dependees) {
+            $count += count($dependees);
         }
+        var_dump($count);
 
-        $consoleStyle = $inceptionResult->getConsoleStyle();
-
-        /** @var DependencyDumper $dependencyDumper */
-        $dependencyDumper = $inceptionResult->getContainer()->getByType(DependencyDumper::class);
-
-        /** @var FileHelper $fileHelper */
-        $fileHelper = $inceptionResult->getContainer()->getByType(FileHelper::class);
-
-        /** @var string[] $analysedPaths */
-        $analysedPaths = $input->getOption('analysed-paths');
-        $analysedPaths = array_map(static function (string $path) use ($fileHelper): string {
-            return $fileHelper->absolutizePath($path);
-        }, $analysedPaths);
-        $dependencies = $dependencyDumper->dumpDependencies(
-            $inceptionResult->getFiles(),
-            static function (int $count) use ($consoleStyle): void {
-                $consoleStyle->progressStart($count);
-            },
-            static function () use ($consoleStyle): void {
-                $consoleStyle->progressAdvance();
-            },
-            count($analysedPaths) > 0 ? $analysedPaths : null
-        );
-        
-        $errors = $this->verify();
-        
-        $consoleStyle->progressFinish();
-        $consoleStyle->writeln(Json::encode($dependencies, Json::PRETTY));
-
-        return $inceptionResult->handleReturn(0);
+        return 0;
     }
 
     /**
      * @param InputInterface $input
-     * @param OutputInterface $output
-     * @return \PHPStan\Command\InceptionResult
-     * @throws \PHPStan\Command\InceptionNotSuccessfulException
-     * @throws \PHPStan\ShouldNotHappenException
+     * @return string[]
      */
-    protected function buildInceptionResult(InputInterface $input, OutputInterface $output): \PHPStan\Command\InceptionResult
+    protected function getAbsolutePaths(InputInterface $input): array
     {
-        /** @var string[] $paths */
         $paths = $input->getArgument('paths');
+        $fileHelper = new FileHelper($this->getCurrentDir());
+        $paths = array_map(function (string $path) use ($fileHelper): string {
+            return $fileHelper->absolutizePath($path);
+        }, $paths);
 
-        /** @var string|null $memoryLimit */
-        $memoryLimit = $input->getOption('memory-limit');
-
-        /** @var string|null $autoloadFile */
-        $autoloadFile = $input->getOption('autoload-file');
-
-        /** @var string|null $configurationFile */
-        $configurationFile = $input->getOption('configuration');
-
-        /** @var string|null $pathsFile */
-        $pathsFile = $input->getOption('paths-file');
-        $inceptionResult = CommandHelper::begin(
-            $input,
-            $output,
-            $paths,
-            $pathsFile,
-            $memoryLimit,
-            $autoloadFile,
-            $configurationFile,
-            '0' // irrelevant but prevents an error when a config file is passed
-        );
-        return $inceptionResult;
+        return $paths;
     }
 
-    protected function verify(): array
+    /**
+     * @return string[]
+     */
+    protected function getAllFilePaths(): array
     {
-        $errors = $this->verifyDirectedAcyclicGraph();
+        $fileFinder = $this->container->getByType(FileFinder::class);
+
+        try {
+            $fileFinderResult = $fileFinder->findFiles($this->paths);
+        } catch (\PHPStan\File\PathNotFoundException $e) {
+            throw new InvalidCommandArgumentException('path was not found: ' . $e->getPath());
+        }
+
+        return $fileFinderResult->getFiles();
+    }
+
+    protected function createContainer(array $paths)
+    {
+        $currentWorkingDirectory = $this->getCurrentDir();
+
+        $tmpDir = sys_get_temp_dir() . '/phpstan';
+        if (!@mkdir($tmpDir, 0777, true) && !is_dir($tmpDir)) {
+            throw new UnexpectedException('creating a temp direcotry is failed: ' . $tmpDir);
+        }
+
+        $containerConfigFiles = [__DIR__ . '/../conf/config.neon'];
+
+        return (new ContainerFactory($currentWorkingDirectory))->create($tmpDir, $containerConfigFiles, $paths);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getCurrentDir(): string
+    {
+        $currentWorkingDirectory = getcwd();
+        if ($currentWorkingDirectory === false) {
+            throw new \PHPStan\ShouldNotHappenException();
+        }
+        return $currentWorkingDirectory;
+    }
+
+    /**
+     * @param string $memoryLimit
+     */
+    protected function setMemoryLimit(string $memoryLimit): void
+    {
+        if (preg_match('#^-?\d+[kMG]?$#i', $memoryLimit) !== 1) {
+            throw new InvalidCommandArgumentException('memory-limit is invalid format "%s".', $memoryLimit);
+        }
+        if (ini_set('memory_limit', $memoryLimit) === false) {
+            throw new UnexpectedException("setting memory_limit to {$memoryLimit} is failed.");
+        }
+    }
+
+    protected function verify(DirectedGraph $graph): array
+    {
+        $errors = $this->verifyDirectedAcyclicGraph($graph);
 
         return $errors;
     }
 
-    protected function verifyDirectedAcyclicGraph()
+    protected function verifyDirectedAcyclicGraph(DirectedGraph $graph)
     {
         // TODO: extract class
+        $detector = new CycleDetector();
+        return $detector->inspect($graph);
     }
 }
---
+++ src/Exceptions/InvalidCommandArgumentException.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+namespace DependencyAnalyzer\Exceptions;
+
+class InvalidCommandArgumentException extends RuntimeException
+{
+}
