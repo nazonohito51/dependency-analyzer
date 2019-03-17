c8b351ea8bd1edc2dcbe1c21f6e908b24875cc28
kawashima <nazonohito51@gmail.com>
2019-03-18 08:57:42 +0900

    remove unused code

--- src/Detector/RuleViolationDetector/DependencyRule.php
+++ src/Detector/RuleViolationDetector/DependencyRule.php
@@ -50,6 +50,7 @@ class DependencyRule
             $depender = $edge->getVertexStart();
             $dependee = $edge->getVertexEnd();
 
+            // TODO: add exclude rule
             if (is_null($this->getGroupName($depender)) || is_null($this->getGroupName($dependee))) {
                 continue;
             }
@@ -61,10 +62,6 @@ class DependencyRule
                     }
                 }
             }
-
-//            if (!$this->isValidVertex($depender, $dependee)) {
-//                $errors[] = "{$depender->getId()}({$this->getGroupName($depender)}) must not depend on {$dependee->getId()}({$this->getGroupName($dependee)}).";
-//            }
         }
 
         return $errors;
@@ -77,55 +74,8 @@ class DependencyRule
                 return $component->getName();
             }
         }
-//        // TODO: add exclude pattern
-//        foreach ($this->definition as $groupName => $groupDefinition) {
-//            if (!$this->matchDefine($vertex, $groupDefinition['define'])) {
-//                continue;
-//            }
-//
-//            return $groupName;
-//        }
 
         return null;
-    }
-
-    protected function matchDefine(Vertex $vertex, array $defines)
-    {
-        foreach ($defines as $define) {
-            $pattern = '/^' . preg_quote($define, '/') . '/';
-            if (preg_match($pattern, $vertex->getId()) !== 1) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    protected function isValidVertex(Vertex $depender, Vertex $dependee): bool
-    {
-        $dependerGroup = $this->getGroupName($depender);
-        $dependeeGroup = $this->getGroupName($dependee);
-
-        if (is_null($dependerGroup) || is_null($dependeeGroup)) {
-            return true;
-        } elseif ($dependerGroup === $dependeeGroup) {
-            return true;
-        } elseif (
-            isset($this->definition[$dependeeGroup]['excludeAnalysis']) &&
-            in_array($depender->getId(), $this->definition[$dependeeGroup]['excludeAnalysis'])
-        ) {
-            return true;
-        }
-
-        if (isset($this->definition[$dependeeGroup]['white']) && !in_array($dependerGroup, $this->definition[$dependeeGroup]['white'])) {
-            return false;
-        }
-
-        if (isset($this->definition[$dependeeGroup]['black']) && in_array($dependerGroup, $this->definition[$dependeeGroup]['black'])) {
-            return false;
-        }
-
-        return true;
     }
 
     public function getDefinition()
