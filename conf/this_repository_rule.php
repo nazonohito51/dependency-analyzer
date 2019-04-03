bac0020171a88f5c616f7f421380e3981d43baed
kawashima <nazonohito51@gmail.com>
2019-04-04 01:23:49 +0900

    update this repository rule

--- conf/this_repository_rule.php
+++ conf/this_repository_rule.php
@@ -1,10 +1,12 @@
 <?php
+$commandsComponent = '\DependencyAnalyzer\Commands\\';
+$graphBuilderComponent = '\DependencyAnalyzer\DependencyGraph\DependencyGraphBuilder';
 $unknownClassReflection = '\DependencyAnalyzer\DependencyDumper\UnknownClassReflection';
 
 return [
     'my dependency rule' => [
         'commands' => [
-            'define' => ['\DependencyAnalyzer\Commands\\'],
+            'define' => [$commandsComponent],
             'description' => 'CLI application commands. control CLI input/output.',
             'depender' => ['!\DependencyAnalyzer\\'],
         ],
@@ -19,12 +21,12 @@ return [
             'depender' => ['dependency_dumper', 'graph_builder']
         ],
         'graph_builder' => [                        // I'm thinking about this component position.
-            'define' => ['\DependencyAnalyzer\DependencyGraph\DependencyGraphBuilder'],
+            'define' => [$graphBuilderComponent],
             'description' => ['Build DependencyGraph'],
             'depender' => ['dependency_dumper']
         ],
         'dependency_graph' => [
-            'define' => ['\DependencyAnalyzer\DependencyGraph', '\DependencyAnalyzer\DependencyGraph\\', '!\DependencyAnalyzer\DependencyGraph\DependencyGraphBuilder'],
+            'define' => ['\DependencyAnalyzer\DependencyGraph', '\DependencyAnalyzer\DependencyGraph\\', "!{$graphBuilderComponent}"],
             'description' => 'Graph of dependencies between classes. Core of this library.',
             'depender' => ['commands', 'dependency_dumper', 'graph_builder', 'inspectors']
         ],
@@ -40,6 +42,39 @@ return [
         'exceptions' => [
             'define' => ['\DependencyAnalyzer\Exceptions\\'],
             'dependee' => ['@php_native']
+        ]
+    ],
+    'relation of external libraries' => [
+        'commands' => [
+            'define' => [$commandsComponent],
+        ],
+        'dependency_dumper' => [
+            'define' => ['\DependencyAnalyzer\DependencyDumper', '\DependencyAnalyzer\DependencyDumper\\'],
+        ],
+        'dependency_graph' => [
+            'define' => ['\DependencyAnalyzer\DependencyGraph', '\DependencyAnalyzer\DependencyGraph\\']
+        ],
+        'inspectors' => [
+            'define' => ['\DependencyAnalyzer\Inspector\\'],
+        ],
+        'other' => [
+            'define' => ['\DependencyAnalyzer\\', "!{$commandsComponent}", '!\DependencyAnalyzer\DependencyGraph', '!\DependencyAnalyzer\DependencyGraph\\', '!\DependencyAnalyzer\DependencyDumper\\', '!\DependencyAnalyzer\Exceptions\\', '!\DependencyAnalyzer\Inspector\\']
+        ],
+        'Symfony Console' => [
+            'define' => ['\Symfony\Component\Console\\'],
+            'depender' => ['commands']
+        ],
+        'PHPStan' => [
+            'define' => ['\PHPStan\\'],
+            'depender' => ['dependency_dumper', 'dependency_graph']
+        ],
+        'PHP Parser' => [
+            'define' => ['\PhpParser\\'],
+            'depender' => ['dependency_dumper']
+        ],
+        'graph' => [
+            'define' => ['\Fhaculty\Graph\\'],
+            'depender' => ['dependency_graph', 'inspectors']        // I will remove inspectors...
         ]
     ]
 ];
