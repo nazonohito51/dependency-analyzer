e33804642cb5a1af18a6ee6e3b5e571bf7830c86
kawashima <nazonohito51@gmail.com>
2019-04-21 13:48:58 +0900

    remove unused code in Visitor

--- src/DependencyDumper/CollectDependenciesVisitor.php
+++ src/DependencyDumper/CollectDependenciesVisitor.php
@@ -7,8 +7,6 @@ use DependencyAnalyzer\DependencyGraph;
 use DependencyAnalyzer\DependencyGraphBuilder;
 use DependencyAnalyzer\Exceptions\ResolveDependencyException;
 use PHPStan\Analyser\Scope;
-use PHPStan\Reflection\ClassReflection;
-use PHPStan\Reflection\Php\PhpFunctionReflection;
 
 /**
  * @canOnlyUsedBy \DependencyAnalyzer\DependencyDumper
@@ -53,95 +51,36 @@ class CollectDependenciesVisitor
 
     public function __invoke(\PhpParser\Node $node, Scope $scope): void
     {
-        try {
-            foreach ($this->dependencyResolver->resolveDependencies($node, $scope, $this->dependencyGraphBuilder) as $dependeeReflection) {
-                if ($node instanceof \PhpParser\Node\Stmt\Class_ ||
-                    $node instanceof \PhpParser\Node\Stmt\Interface_ ||
-                    $node instanceof \PhpParser\Node\Stmt\ClassMethod ||
-                    $node instanceof \PhpParser\Node\Expr\Closure ||
-                    $node instanceof \PhpParser\Node\Expr\FuncCall ||
-                    $node instanceof \PhpParser\Node\Expr\MethodCall ||
-                    $node instanceof \PhpParser\Node\Expr\PropertyFetch ||
-                    $node instanceof \PhpParser\Node\Expr\StaticCall ||
-                    $node instanceof \PhpParser\Node\Expr\ClassConstFetch ||
-                    $node instanceof \PhpParser\Node\Expr\StaticPropertyFetch ||
-                    $node instanceof \PhpParser\Node\Expr\New_ ||
-                    $node instanceof \PhpParser\Node\Stmt\TraitUse ||
-                    $node instanceof \PhpParser\Node\Expr\Instanceof_ ||
-                    $node instanceof \PhpParser\Node\Stmt\Catch_ ||
-                    $node instanceof \PhpParser\Node\Expr\ArrayDimFetch ||
-                    $node instanceof \PhpParser\Node\Stmt\Foreach_ ||
-                    $node instanceof \PhpParser\Node\Expr\Array_ ||
-                    $node instanceof \PhpParser\Node\Stmt\PropertyProperty) {
-                    continue;
-                }
+        // TODO: collect dependencies to file cache for improve performance
 
-                if ($dependeeReflection instanceof ClassReflection) {
-//                    if ($node instanceof MethodCall && $scope->getFunction()) {
-//                        $dependeeClass = $dependeeReflection;
-//                        $dependeeFunction = $node->name->name;
-//
-//                        $depender = $scope->getClassReflection();
-//                        $dependerFunction = $scope->getFunction();
-//                    }
-                    $this->addDependency($node, $scope, $dependeeReflection);
-                } elseif ($dependeeReflection instanceof UnknownClassReflection) {
-                    $this->addUnknownDependency($node, $scope, $dependeeReflection);
-                } elseif ($dependeeReflection instanceof PhpFunctionReflection) {
-                    // function call
-                    // ex:
-                    //   array_map(...);
-                    //   var_dump(...);
-                } else {
-                    // error of DependencyResolver
-                    throw new ResolveDependencyException($node, 'resolving node dependency is failed.');
-                }
-            }
+        try {
+            $this->dependencyResolver->resolveDependencies($node, $scope, $this->dependencyGraphBuilder);
+//            foreach ($this->dependencyResolver->resolveDependencies($node, $scope, $this->dependencyGraphBuilder) as $dependeeReflection) {
+//                if ($dependeeReflection instanceof ClassReflection) {
+////                    if ($node instanceof MethodCall && $scope->getFunction()) {
+////                        $dependeeClass = $dependeeReflection;
+////                        $dependeeFunction = $node->name->name;
+////
+////                        $depender = $scope->getClassReflection();
+////                        $dependerFunction = $scope->getFunction();
+////                    }
+//                    $this->addDependency($node, $scope, $dependeeReflection);
+//                } elseif ($dependeeReflection instanceof UnknownClassReflection) {
+//                    $this->addUnknownDependency($node, $scope, $dependeeReflection);
+//                } elseif ($dependeeReflection instanceof PhpFunctionReflection) {
+//                    // function call
+//                    // ex:
+//                    //   array_map(...);
+//                    //   var_dump(...);
+//                } else {
+//                    // error of DependencyResolver
+//                    throw new ResolveDependencyException($node, 'resolving node dependency is failed.');
+//                }
+//            }
         } catch (ResolveDependencyException $e) {
             if ($this->observer) {
                 $this->observer->notifyResolveDependencyError($this->file, $e);
             }
-        }
-    }
-
-    protected function addDependency(\PhpParser\Node $node, Scope $scope, ClassReflection $dependeeReflection): void
-    {
-        if ($scope->isInClass()) {
-            if ($scope->getClassReflection()->getDisplayName() === $dependeeReflection->getDisplayName()) {
-                // call same class method/property
-            } else {
-                $this->dependencyGraphBuilder->addDependency(
-                    $scope->getClassReflection()->getNativeReflection(),
-                    $dependeeReflection->getNativeReflection()
-                );
-            }
-        } else {
-            // Maybe, class declare statement
-            // ex:
-            //   class Hoge {}
-            //   abstract class Hoge {}
-            //   interface Hoge {}
-            if ($node instanceof \PhpParser\Node\Stmt\ClassLike) {
-                $dependerReflection = $this->dependencyResolver->resolveClassReflection($node->namespacedName->toString());
-                if ($dependerReflection instanceof ClassReflection) {
-                    $this->dependencyGraphBuilder->addDependency(
-                        $dependerReflection->getNativeReflection(),
-                        $dependeeReflection->getNativeReflection()
-                    );
-                } else {
-                    throw new ResolveDependencyException($node, 'resolving node dependency is failed, because unexpected node.');
-                }
-            }
-        }
-    }
-
-    protected function addUnknownDependency(\PhpParser\Node $node, Scope $scope, UnknownClassReflection $classReflection)
-    {
-        if ($scope->isInClass()) {
-            $this->dependencyGraphBuilder->addUnknownDependency(
-                $scope->getClassReflection()->getNativeReflection(),
-                $classReflection->getDisplayName()
-            );
         }
     }
 
