76a5553834cc234bbb247a409df008d7cebb20ad
kawashima <nazonohito51@gmail.com>
2019-04-27 11:50:41 +0900

    rename \Tests\Integration to \Tests\Component

--- tests/Component/InspectorTest.php
+++ tests/Component/InspectorTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Tests\Integration;
+namespace Tests\Component;
 
 use DependencyAnalyzer\DependencyGraph;
 use DependencyAnalyzer\Inspector\RuleViolationDetector;
