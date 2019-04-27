c14b2a09a657e8b1c6a6950811b97002e32410e4
kawashima <nazonohito51@gmail.com>
2019-04-27 22:52:39 +0900

    add ClassNameMatcherTest

---
+++ tests/Unit/Patterns/ClassNameMatcherTest.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Unit\DependencyAnalyzer\Matcher;
+
+use DependencyAnalyzer\Matcher\ClassNameMatcher;
+use Tests\TestCase;
+
+class ClassNameMatcherTest extends TestCase
+{
+    public function provideIsMatch()
+    {
+        return [
+            'class1' => [['\Tests\Fixtures\ClassMatcher\SomeClass1'], 'Tests\Fixtures\ClassMatcher\SomeClass1', true],
+            'class2' => [['\Tests\Fixtures\ClassMatcher\SomeClass1'], 'Tests\Fixtures\ClassMatcher\SomeClass2', false],
+            'namespace1' => [['\Tests\Fixtures\ClassMatcher\\'], 'Tests\Fixtures\ClassMatcher\SomeClass1', true],
+            'namespace2' => [['\Tests\Fixtures\ClassMatcher\\'], 'Tests\Fixtures\DifferentNameSpace\SomeClass1', false],
+            'exclude class1' => [['!\Tests\Fixtures\ClassMatcher\SomeClass1'], 'Tests\Fixtures\ClassMatcher\SomeClass1', false],
+            'exclude class2' => [['!\Tests\Fixtures\ClassMatcher\SomeClass1'], 'Tests\Fixtures\ClassMatcher\SomeClass2', true],
+            'exclude namespace1' => [['!\Tests\Fixtures\ClassMatcher\\'], 'Tests\Fixtures\ClassMatcher\SomeClass1', false],
+            'exclude namespace2' => [['!\Tests\Fixtures\ClassMatcher\\'], 'Tests\Fixtures\DifferentNameSpace\SomeClass1', true],
+            'have multi pattern1' => [['\Tests\Fixtures\ClassMatcher\SomeClass1', '\Tests\Fixtures\ClassMatcher\SomeClass2'], 'Tests\Fixtures\ClassMatcher\SomeClass1', true],
+            'have multi pattern2' => [['\Tests\Fixtures\ClassMatcher\SomeClass1', '\Tests\Fixtures\ClassMatcher\SomeClass2'], 'Tests\Fixtures\ClassMatcher\SomeClass2', true],
+            'have multi pattern3' => [['\Tests\Fixtures\ClassMatcher\SomeClass1', '\Tests\Fixtures\ClassMatcher\SomeClass2'], 'Tests\Fixtures\ClassMatcher\SomeClass3', false],
+            'have multi pattern4' => [['\Tests\Fixtures\ClassMatcher\\', '!\Tests\Fixtures\ClassMatcher\SomeClass2'], 'Tests\Fixtures\ClassMatcher\SomeClass1', true],
+            'have multi pattern5' => [['\Tests\Fixtures\ClassMatcher\\', '!\Tests\Fixtures\ClassMatcher\SomeClass2'], 'Tests\Fixtures\ClassMatcher\SomeClass2', false],
+        ];
+    }
+
+    /**
+     * @param array $patterns
+     * @param string $target
+     * @param bool $expected
+     * @dataProvider provideIsMatch
+     */
+    public function testIsMatch(array $patterns, string $target, bool $expected)
+    {
+        $matcher = new ClassNameMatcher($patterns);
+
+        $this->assertSame($expected, $matcher->isMatch($target));
+    }
+}
