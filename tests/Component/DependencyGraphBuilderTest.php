67ddb134d2b1df55c9a2968c3342adc88b37a9b0
kawashima <nazonohito51@gmail.com>
2019-04-27 12:14:29 +0900

    move DependencyGraphBuilderTest to Component test

--- tests/Component/DependencyGraphBuilderTest.php
+++ tests/Component/DependencyGraphBuilderTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Tests\Unit\DependencyAnalyzer;
+namespace Tests\Component;
 
 use DependencyAnalyzer\DependencyGraph;
 use DependencyAnalyzer\DependencyGraph\DependencyTypes\ConstantFetch;
